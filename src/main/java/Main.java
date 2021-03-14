47180235d46cd1e873a32055a14428430937151f
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-03-14 15:22:34 +0200

    Method add for 4 params added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,11 @@ public class Main {
         return a + b + c;
     }
 
+    public int add(int a, int b, int c, int d) {
+        System.out.println("This is add method for 4 params");
+        return a + b + c + d;
+    }
+
     public int sub(int a, int b) {
         return a - b;
     }
