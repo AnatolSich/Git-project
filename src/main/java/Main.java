1274439fd8029e1c555767a92e033a1442ff9a5f
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-03-14 14:09:57 +0200

    Methods sub div mult sqrt added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(double a) {
+        return Math.sqrt(a);
+    }
 }
